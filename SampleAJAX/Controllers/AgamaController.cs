412674230a872b27c0e621e632c07082b9ebafca
Erick Kurniawan <erick.kurniawan@gmail.com>
2017-03-30 10:41:34 +0700

    menambahkan jquery ui

--- SampleAJAX/Controllers/AgamaController.cs
+++ SampleAJAX/Controllers/AgamaController.cs
@@ -13,5 +13,10 @@ namespace SampleAJAX.Controllers
         {
             return View();
         }
+
+        public ActionResult SampleJQueryUI()
+        {
+            return View();
+        }
     }
 }
